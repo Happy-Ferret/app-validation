25b731d0731135e920755cb6850bc94f7091993c
boo1ean <boo1ean0807@gmail.com>
2014-11-18 23:07:17 +0100

    Add more tests

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var should = require('should');
 var validate = require('./');
+var _ = require('lodash');
 
 describe('Validate', function () {
 
@@ -53,5 +54,65 @@ describe('Validate', function () {
 			data.name.should.be.exactly('not-haha');
 			done();
 		}).catch(done);
+	});
+
+	validate.extendRules({
+		'gmail': function (email) {
+			if (email.indexOf('@gmail.com') === -1) {
+				return 'error-email';
+			}
+		},
+
+		'password': function (password) {
+			if (password.length < 7) {
+				return 'error-password';
+			}
+		}
+	});
+
+	var validator = validate({
+		email: ['required', 'gmail'],
+		password: ['required', 'password']
+	});
+
+	it('should fail with multiple rules', function (done) {
+		var data = {
+			email: 'ema@email.com',
+			password: 'lol',
+			stuff: 123
+		};
+
+		validator(data).catch(function (err) {
+			err.errors.email.should.be.equal('error-email');
+			err.errors.password.should.be.equal('error-password');
+			done();
+		});
+	});
+
+	it('should fail with multiple rules required ruel', function (done) {
+		var data = {
+			email: '',
+			password: null,
+			stuff: 1
+		};
+
+		validator(data).catch(function (err) {
+			err.errors.email.should.be.equal('Required field');
+			err.errors.password.should.be.equal('Required field');
+			done();
+		});
+	});
+
+	it('should pass with multiple rules', function (done) {
+		var data = {
+			email: 'asdf@gmail.com',
+			password: '123456789',
+			stuff: 'okok'
+		};
+
+		validator(data).then(function (data2) {
+			_.isEqual(data, data2).should.be.ok;
+			done();
+		});
 	});
 });
